b39e2265c5df4bcc81dc4aa4fc328dc464aab16a
Yonathan Barrantes <ybarrantes.juntos085@gmail.com>
2024-02-09 13:47:01 +0100

    Added program implementation to openclosed problem project

--- OpenClosed.Problem/Program.cs
+++ OpenClosed.Problem/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using OpenClosed.Problem;
+
+var reportingService = new ReportingService();
+var orders = new List<Order>
+{
+    new Order { Description = "Order 1", Total = 100 },
+    new Order { Description = "Order 2", Total = 200 },
+    new Order { Description = "Order 3", Total = 300 }
+};
+
+reportingService.GenerateReport(orders, ReportingService.ReportType.PDF);
+reportingService.GenerateReport(orders, ReportingService.ReportType.Excel);
