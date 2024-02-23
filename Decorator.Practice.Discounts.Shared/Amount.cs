470d9193cfb9924ad8542bdb5aed7a2c7a4dc8f3
Yonathan Barrantes <ybarrantes.juntos085@gmail.com>
2024-02-23 08:56:48 +0100

    Changed decorator discounts practice project name

---
+++ Decorator.Practice.Discounts.Shared/Amount.cs
@@ -0,0 +1,7 @@
+﻿namespace Decorator.Practice.Discounts.Shared;
+
+public struct Amount
+{
+    public decimal Value { get; set; }
+    public string? CurrencyCode { get; set; }
+}
---
+++ Decorator.Practice.Discounts.Shared/Price.cs
@@ -0,0 +1,7 @@
+﻿namespace Decorator.Practice.Discounts.Shared;
+
+public class Price
+{
+    public Amount? BaseAmount { get; set; }
+    public Amount? TotalAmount { get; set; }
+}
