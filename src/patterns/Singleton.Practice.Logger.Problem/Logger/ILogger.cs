900e329fc33c704c455318fb6ba30e3a9223c4d9
Yonathan Barrantes <ybarrantes.juntos085@gmail.com>
2024-02-23 13:21:56 +0100

    Added logger namespace

---
+++ src/patterns/Singleton.Practice.Logger.Problem/Logger/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Singleton.Practice.Logger.Problem.Log;
+
+public interface ILogger
+{
+    void LogDebug(string message);
+    void LogInfo(string message);
+    void LogError(string message);
+    void LogWarning(string message);
+    void Flush();
+}
---
+++ src/patterns/Singleton.Practice.Logger.Problem/Logger/Logger.cs
@@ -0,0 +1,44 @@
+﻿namespace Singleton.Practice.Logger.Problem.Log;
+
+public class Logger : ILogger
+{
+    private readonly Queue<string> _logQueue = new();
+    private readonly Guid _id = Guid.NewGuid();
+    
+    public void LogDebug(string message)
+    {
+        EnqueueMessage($"DEBUG {message}");
+    }
+    
+    public void LogInfo(string message)
+    {
+        EnqueueMessage($"INFO {message}");
+    }
+    
+    public void LogError(string message)
+    {
+        EnqueueMessage($"ERROR {message}");
+    }
+
+    public void LogWarning(string message)
+    {
+        EnqueueMessage($"WARNING {message}");
+    }
+
+    public void Flush()
+    {
+        Console.WriteLine($"Flushing log messages for logger {_id}");
+        
+        while (_logQueue.Count != 0)
+        {
+            var message = _logQueue.Dequeue();
+            Console.WriteLine(message);
+        }
+    }
+    
+    private void EnqueueMessage(string message)
+    {
+        var messageToLog = $"{DateTime.Now}: {message}";
+        _logQueue.Enqueue(messageToLog);
+    }
+}
